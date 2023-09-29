afe1745b380a5dd361844db39bf2d0615d3cc5f8
DSW <elmcwl@gmail.com>
2023-09-29 00:29:01 -0700

    syle: progress select

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -14,6 +14,18 @@ const Select = () => {
             <i className="uil uil-search"></i>
             <input type="text" placeholder='Search' />
           </div>
+          <ul className="options">
+            <li>Australia</li>
+            <li>Colombia</li>
+            <li>Dinamarca</li>
+            <li>Alemania</li>
+            <li>Indonesia</li>
+            <li>Australia</li>
+            <li>Colombia</li>
+            <li>Dinamarca</li>
+            <li>Alemania</li>
+            <li>Indonesia</li>
+          </ul>
         </div>
       </div>
     </div>
