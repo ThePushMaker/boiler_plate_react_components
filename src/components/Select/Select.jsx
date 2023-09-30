ca0f23bcfaf3dfca81c50e76991ad5e67c5f1017
DSW <elmcwl@gmail.com>
2023-09-29 21:50:16 -0700

    feat: select show selected item and  autoclose when clicked outside

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,44 +1,67 @@
 import './Select.css'
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const Select = () => {
-  
   const [isActive, setIsActive] = useState(false);
   const [selectBtn, setSelectBtn] = useState("Select Country");
   const [searchInp, setSearchInp] = useState('');
-  const [filteredCountries, setFilteredCountries] = useState([]); // Nuevo estado para almacenar los países filtrados
-  
-  
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const wrapperRef = useRef(null); // Ref para el div contenedor del select
+
   const toggleSelect = () => {
     setIsActive(!isActive);
   }
-  
+
+  const closeSelect = () => {
+    setIsActive(false);
+  }
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      closeSelect();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isActive && selectedCountry) {
+      setSelectBtn(selectedCountry);
+    }
+  }, [isActive, selectedCountry]);
+
   const countries = ["Australia", "Colombia", "Dinamarca", "Alemania", "Indonesia","Australia", "Colombia", "Dinamarca", "Alemania", "Indonesia"];
 
   const updateName = (selectedLi) => {
     setSelectBtn(selectedLi)
-    setIsActive(false); // Cerrar la lista después de seleccionar un país
+    setSelectedCountry(selectedLi);
+    closeSelect();
   }
 
-  // Función para manejar cambios en el input de búsqueda
   const handleSearchInputChange = (event) => {
     const inputValue = event.target.value;
     setSearchInp(inputValue);
-    
+
     const searchedVal = inputValue.toLowerCase();
-    // Filtrar los países que coinciden con la búsqueda
     const filtered = countries.filter(data => {
-      return data.toLowerCase().includes(searchedVal); // Usar includes para buscar en cualquier parte del nombre
+      return data.toLowerCase().includes(searchedVal);
     })
-    
-    setFilteredCountries(filtered); // Actualizar el estado de países filtrados
-}
 
-  
+    setFilteredCountries(filtered);
+    setHasSearched(true);
+  }
 
-  return(
+  return (
     <div className='mt-5'>
-      <div className={`wrapper ${isActive ? 'active' : ''}`}>
+      <div className={`wrapper ${isActive ? 'active' : ''}`} ref={wrapperRef}>
         <div className="select-btn" onClick={toggleSelect}>
           <span>{selectBtn}</span>
           <i className="uil uil-angle-down"></i>
@@ -46,14 +69,34 @@ const Select = () => {
         <div className="content">
           <div className="search">
             <i className="uil uil-search"></i>
-            <input type="text" placeholder='Search' value={searchInp} onChange={handleSearchInputChange}/>
+            <input type="text" placeholder='Search' value={searchInp} onChange={handleSearchInputChange} />
           </div>
           <ul className="options">
-            {filteredCountries.map((item, index) => (
-              <li key={index} onClick={() => updateName(item)}>
-                {item}
-              </li>
-            ))}
+            {hasSearched ? (
+              filteredCountries.length > 0 ? (
+                filteredCountries.map((item, index) => (
+                  <li
+                    key={index}
+                    onClick={() => updateName(item)}
+                    className={selectedCountry === item ? 'selected' : ''}
+                  >
+                    {item}
+                  </li>
+                ))
+              ) : (
+                <p>Oops! Country not found</p>
+              )
+            ) : (
+              countries.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => updateName(item)}
+                  className={selectedCountry === item ? 'selected' : ''}
+                >
+                  {item}
+                </li>
+              ))
+            )}
           </ul>
         </div>
       </div>
